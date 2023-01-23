c3eb7b4db3df7297cff21799013d64ea867249b3
Flamefire <flammewow@live.dk>
2023-01-23 10:59:43 +0100

    step-4

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ void main() {
       routes: {
         "/login/": (context) => const LoginView(),
         "/register/": (context) => const RegisterView(),
+        "/home/": (context) => const HomeView(),
       },
     ),
   );
